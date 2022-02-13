410c5233ad894618a3d5582a587fae2808455f9a
Ransford Larbi <42917506+iamthe-observer@users.noreply.github.com>
2022-02-13 22:29:47 +0000

    learning the modules of node

---
+++ app.js
@@ -0,0 +1,33 @@
+// const log = require('./logger')
+
+// log('john')
+
+// path module
+const path = require('path');
+let pathObj = path.parse(__filename)
+
+// console.log(pathObj);
+
+
+// OS module
+const os = require('os');
+const {
+    createRequire
+} = require('module');
+
+let totalMemory = os.totalmem()
+let freeMemory = os.freemem()
+
+// console.log(totalMemory, freeMemory);
+
+
+// File System module
+const fs = require('fs');
+
+const files = fs.readdirSync("./")
+    // console.log(files);
+
+const files2 = fs.readdir('./', (err, files) => {
+    if (err) console.log("error", err);
+    else console.log(('results', files));
+})
