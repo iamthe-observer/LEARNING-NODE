a20c0db4cb4cb64607cf477bd49f693218127131
Ransford Larbi <42917506+iamthe-observer@users.noreply.github.com>
2022-02-14 11:09:32 +0000

    currently learning 8hr video for node

--- app.js
+++ app.js
@@ -3,29 +3,31 @@
 // log('john')
 
 // path module
-const path = require('path');
-let pathObj = path.parse(__filename)
+// const path = require('path');
+// let pathObj = path.parse(__filename)
 
 // console.log(pathObj);
 
 
-// OS module
-const os = require('os');
-const {
-    createRequire
-} = require('module');
 
-let totalMemory = os.totalmem()
-let freeMemory = os.freemem()
+// OS module
+// const os = require('os');
+// const {
+//     createRequire
+// } = require('module');
+
+// let totalMemory = os.totalmem()
+// let freeMemory = os.freemem()
 
 // console.log(totalMemory, freeMemory);
 
 
-// File System module
-const fs = require('fs');
 
-const files = fs.readdirSync("./")
-    // console.log(files);
+// File System module
+// const fs = require('fs');
+
+// const files = fs.readdirSync("./")
+// console.log(files);
 
 // const files2 = fs.readdir('./', (err, files) => {
 //     if (err) console.log("error", err);
@@ -33,13 +35,34 @@ const files = fs.readdirSync("./")
 // })
 
 
+
 // Events
-const Log = require('./logger')
-const logger = new Log()
+// const Log = require('./logger')
+// const logger = new Log()
 
-// Register a listener
-logger.on("messageLogged", (arg) => {
-    console.log('listener called', arg);
-})
+// // Register a listener
+// logger.on("messageLogged", (arg) => {
+//     console.log('listener called', arg);
+// })
 
-logger.log('message')
+// logger.log('message')
+
+
+// Http module
+// 127.0.0.1:3000
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//         if (req.url === '/') {
+//             res.write('hello world')
+//             res.write('so this is love');
+//             console.log('end connection');
+//             res.end()
+//         }
+//     })
+//     // register a server listener
+// server.on('connection', (socket) => {
+//     console.log("new connection");
+// })
+// server.listen(3000)
+
+// console.log('listening on port 3000...');
---
+++ module.js
@@ -0,0 +1,14 @@
+// const EventEmitter = require('events');
+// class Logger extends EventEmitter {
+//     log(message) {
+//         console.log(message)
+
+//         // Raise an event
+//         this.emit("messageLogged", {
+//             data: message
+//         })
+//     }
+
+// }
+
+// module.exports = Logger
